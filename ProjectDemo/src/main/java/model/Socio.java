5a77bfdbd066c9209fdfba38b0cb803dc54d5f54
Usuario <Usuario@DESKTOP-GK3OIP8>
2022-04-07 16:50:48 -0300

    completar

--- ProjectDemo/src/main/java/model/Socio.java
+++ ProjectDemo/src/main/java/model/Socio.java
@@ -2,4 +2,19 @@ package model;
 
 public class Socio {
 
+	private String name;
+	private String apellido;
+	
+	public Socio() {
+
+	}
+
+	public Socio(String name, String apellido) {
+		super();
+		this.name = name;
+		this.apellido = apellido;
+	}
+
+	
+	
 }
