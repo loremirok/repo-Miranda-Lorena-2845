6f10c586422b45f0b789061a9c84104608569d2d
Usuario <Usuario@DESKTOP-GK3OIP8>
2022-04-07 16:03:23 -0300

    Agregue datos

--- ProjectDemo/src/main/java/model/Autor.java
+++ ProjectDemo/src/main/java/model/Autor.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Autor {
 
+public class Autor {
+	private String nombre;
+	private String apellido;
+	private int id;
+	
+	public Autor() {
+		
+	}
+
+	public Autor(String nombre, String apellido, int id) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.id = id;
+	}	
+
+	
 }
+
+
